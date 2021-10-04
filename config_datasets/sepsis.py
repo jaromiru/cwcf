b4c09f2f0a322b01f1c87fb2fb97d9203adfdba6
glennmatlin <glenn.matlin@gmail.com>
2021-10-04 17:45:25 -0400

    sepsis shock configs

---
+++ config_datasets/sepsis.py
@@ -0,0 +1,8 @@
+DATASET = 'sepsis'
+
+CLASSES  = 2
+FEATURES = 234
+
+NN_SIZE    = 256
+DIFFICULTY = 100#10000
+
---
+++ config_datasets/shock.py
@@ -0,0 +1,8 @@
+DATASET = 'shock'
+
+CLASSES  = 2
+FEATURES = 234
+
+NN_SIZE    = 256
+DIFFICULTY = 10000
+
