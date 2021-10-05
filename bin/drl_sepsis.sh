83fc330c381d3e6acaa4fc40d8a63c46338c173c
Glenn Matlin <glenn.matlin@gmail.com>
2021-10-04 22:53:45 -0400

    fix tmux session names

--- bin/drl_sepsis.sh
+++ bin/drl_sepsis.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 #[1, 0.1, 0.01, 0.001, 0.0001]
 
-tmux new-session -d -s "sepsis_1.0_3" bash ~/cwcf/bin/drl.sh sepsis 1.0 3
-tmux new-session -d -s "sepsis_0.1_4" bash ~/cwcf/bin/drl.sh sepsis 0.1 4
-tmux new-session -d -s "sepsis_0.01_5" bash ~/cwcf/bin/drl.sh sepsis 0.01 5
-tmux new-session -d -s "sepsis_0.001_6" bash ~/cwcf/bin/drl.sh sepsis 0.001 6
-tmux new-session -d -s "sepsis_0.0001_7" bash ~/cwcf/bin/drl.sh sepsis 0.0001 7
+tmux new-session -d -s "sepsis-1_0-3" bash ~/cwcf/bin/drl.sh sepsis 1.0 3
+tmux new-session -d -s "sepsis-0_1-4" bash ~/cwcf/bin/drl.sh sepsis 0.1 4
+tmux new-session -d -s "sepsis-0_01-5" bash ~/cwcf/bin/drl.sh sepsis 0.01 5
+tmux new-session -d -s "sepsis-0_001-6" bash ~/cwcf/bin/drl.sh sepsis 0.001 6
+tmux new-session -d -s "sepsis-0_0001-7" bash ~/cwcf/bin/drl.sh sepsis 0.0001 7
--- bin/drl_shock.sh
+++ bin/drl_shock.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 #[1, 0.1, 0.01, 0.001, 0.0001]
 
-tmux new-session -d -s "shock_1.0_3" bash ~/cwcf/bin/drl.sh shock 1.0 3
-tmux new-session -d -s "shock_0.1_4" bash ~/cwcf/bin/drl.sh shock 0.1 4
-tmux new-session -d -s "shock_0.01_5" bash ~/cwcf/bin/drl.sh shock 0.01 5
-tmux new-session -d -s "shock_0.001_6" bash ~/cwcf/bin/drl.sh shock 0.001 6
-tmux new-session -d -s "shock_0.0001_7" bash ~/cwcf/bin/drl.sh shock 0.0001 7
+tmux new-session -d -s "shock-1_0-3" bash ~/cwcf/bin/drl.sh shock 1.0 3
+tmux new-session -d -s "shock-0_1-4" bash ~/cwcf/bin/drl.sh shock 0.1 4
+tmux new-session -d -s "shock-0_01-5" bash ~/cwcf/bin/drl.sh shock 0.01 5
+tmux new-session -d -s "shock-0_001-6" bash ~/cwcf/bin/drl.sh shock 0.001 6
+tmux new-session -d -s "shock-0_0001-7" bash ~/cwcf/bin/drl.sh shock 0.0001 7
