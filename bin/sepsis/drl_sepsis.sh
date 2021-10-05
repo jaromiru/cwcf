3a1032b4e6744ea3a2f190587dd6a6d252131bf2
Glenn Matlin <glenn.matlin@gmail.com>
2021-10-04 21:53:46 -0400

    folders for shell scripts, and move matlab code into output

--- bin/sepsis/drl_sepsis.sh
+++ bin/sepsis/drl_sepsis.sh
@@ -1,8 +1,16 @@
 #!/usr/bin/env bash
 
-Ds='shock'
-La=1.0
+# Parameters
+Ds='sepsis'
+La=0.01
+
+# Output Path
 filepath="/home/gmatlin3/cwcf/output/${Ds}/flambda${La}/"
 mkdir -p $filepath
+
+# Job Execution
 CUDA_VISIBLE_DEVICES=7 python -u main.py --dataset $Ds --flambda $La --use_hpc 1 --pretrain 1 \
 | tee "${filepath}drl-${Ds}-flambda${La}.log"
+
+# MATLAB Eval Code
+cp "/home/gmatlin3/cwcf/tools/debug.m" $filepath
