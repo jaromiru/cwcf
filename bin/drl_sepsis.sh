29d7da19ef93db8f49a53ff29517b98f3a55ae76
Glenn Matlin <glenn.matlin@gmail.com>
2021-10-04 22:49:27 -0400

    experimentation distribution scripts

--- bin/drl_sepsis.sh
+++ bin/drl_sepsis.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 #[1, 0.1, 0.01, 0.001, 0.0001]
 
-tmux new-session -d -s "sepsis_1.0_0" bash ~/cwcf/bin/drl.sh sepsis 1.0 0
-tmux new-session -d -s "sepsis_0.1_1" bash ~/cwcf/bin/drl.sh sepsis 0.1 1
-tmux new-session -d -s "sepsis_0.01_2" bash ~/cwcf/bin/drl.sh sepsis 0.01 2
-tmux new-session -d -s "sepsis_0.001_3" bash ~/cwcf/bin/drl.sh sepsis 0.001 3
-tmux new-session -d -s "sepsis_0.0001_4" bash ~/cwcf/bin/drl.sh sepsis 0.0001 4
+tmux new-session -d -s "sepsis_1.0_3" bash ~/cwcf/bin/drl.sh sepsis 1.0 3
+tmux new-session -d -s "sepsis_0.1_4" bash ~/cwcf/bin/drl.sh sepsis 0.1 4
+tmux new-session -d -s "sepsis_0.01_5" bash ~/cwcf/bin/drl.sh sepsis 0.01 5
+tmux new-session -d -s "sepsis_0.001_6" bash ~/cwcf/bin/drl.sh sepsis 0.001 6
+tmux new-session -d -s "sepsis_0.0001_7" bash ~/cwcf/bin/drl.sh sepsis 0.0001 7
